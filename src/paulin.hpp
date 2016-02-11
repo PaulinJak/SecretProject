496ddc09ca39df6188364f87b9ba057d262d6a62
PaulinJak <jacquot.paulin@gmail.com>
2016-02-11 22:01:44 +0100

    up

--- src/paulin.hpp
+++ src/paulin.hpp
@@ -8,8 +8,13 @@
 #include "matrix.hpp"
 #include <vector>
 #include "objects.hpp"
-
-
+#include <math.h>
+#include <stdlib.h>
+#include "basics.hpp"
 using namespace std;
 
+
+Warehouse findNearestWarehouse(Case const& pos,Instance const& instance);
+
+void initialSend(ofstream& outPut, vector<Case>& drones_positions, vector<int>& drones_temps, Instance& instance);
 #endif
