553f4193088d1d5a37773bb093916c2e875da042
mauddoum <maud.doum@gmail.com>
2016-02-11 18:26:13 +0100

    gna gna gna paulin, voila.

---
+++ src/maud.cpp
@@ -0,0 +1,18 @@
+//
+//  m.cpp
+//
+//
+//  Created by Maud Doumergue on 10/02/2016.
+//
+//
+
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include "matrix.hpp"
+#include "objects.hpp"
+#include "basics.hpp"
+
+using namespace std;
+
---
+++ src/maud.hpp
@@ -0,0 +1,23 @@
+//
+//  m.hpp
+//
+//
+//  Created by Maud Doumergue on 10/02/2016.
+//
+//
+
+#ifndef fonctions_hpp
+#define fonctions_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include "matrix.hpp"
+#include "objects.hpp"
+#include "basics.hpp"
+
+using namespace std;
+
+#endif /* fonctions_hpp */
