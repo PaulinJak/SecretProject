173723d1ff14e64ead8365341021a8380dc1cf11
Jean Perier <jean.perier.polytechnique@gmail.com>
2016-02-11 18:24:06 +0100

    Ajout du fichier basics

---
+++ src/basics.hpp
@@ -0,0 +1,12 @@
+#ifndef __BASIC_HPP_
+#define __BASIC_HPP_
+
+#include "matrix.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+void readInstance(const char* nom_fichier,int& n, int& m, Matrix2D<int>& image );
+
+#endif /*__BASIC_HPP_*/
