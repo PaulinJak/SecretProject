b8bc92271efdb9ae0ad4b342b0dab2294d8c8688
PaulinJak <jacquot.paulin@gmail.com>
2016-02-11 22:50:18 +0100

    kvnkvanzaaaaaaaaaaa

--- src/paulin.cpp
+++ src/paulin.cpp
@@ -15,7 +15,7 @@ using namespace std;
 Warehouse findNearestWarehouse(Case const& pos,Instance const& instance){
   int bestDistance =distance(pos,(instance.warehouses[0]).position);
    int nearestWarehouseId =0;
-   for(int i=1; i<<instance.warehouses.size(); i++){
+   for(int i=1; i<instance.warehouses.size(); i++){
 int newdist=distance(pos,(instance.warehouses[i]).position);
      if (newdist<bestDistance){
        bestDistance=newdist;
@@ -29,14 +29,12 @@ int newdist=distance(pos,(instance.warehouses[i]).position);
 void initialSend(ofstream& outPut, vector<Case>& drones_positions, vector<int>& drones_temps, Instance& instance){
 
   vector<Case> uniformPositions(instance.n_drones,Case(0,0));
-
-for (int drone=0; drone< instance.n_drones ;drone++){
+for (int drone=0; drone< uniformPositions.size() ;drone++){
 uniformPositions[drone].i=rand() % instance.n;
 uniformPositions[drone].j=rand() %instance.m;
 }
 
 // double area = instance.n*instance.m/instance.n_drones;
-
 
 for (int drone=0; drone< instance.n_drones;drone++){
      
@@ -45,7 +43,6 @@ Warehouse wareh_id_to_send=findNearestWarehouse( uniformPositions[drone],instanc
 outPut <<drone <<" L "<< wareh_id_to_send.w<< "0 0\n";
 
 int distance_to_fly=distance(drones_positions[drone],wareh_id_to_send.position);
-
 drones_positions[drone]=wareh_id_to_send.position;
 
 drones_temps[drone]+=distance_to_fly+1;
