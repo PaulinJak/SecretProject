1670cd0a9ee330c2f9b5ae2c13c82e885a2b99d1
PaulinJak <jacquot.paulin@gmail.com>
2016-02-11 20:00:37 +0100

    basisc header

--- src/basics.hpp
+++ src/basics.hpp
@@ -5,8 +5,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "objects.hpp"
 
 
-void readInstance(const char* nom_fichier,int& n, int& m, Matrix2D<int>& image );
+void readInstance(const char* nom_fichier,Instance& instance );
 
 #endif /*__BASIC_HPP_*/
